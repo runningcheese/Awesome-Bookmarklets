f2e2929619f61e5571e0c6651159530a12da47dd
RunningChese <runningcheese@qq.com>
2022-05-10 01:06:48 +0800

    Update jiathisqr.js

--- libs/jiathisqr.js
+++ libs/jiathisqr.js
@@ -68,7 +68,7 @@ var qrcode = document.createElement("img");
 	qrcode.setAttribute('class','shaunsational_qr');
 	qrcode.setAttribute('id','qrcode');
 	qrcode.setAttribute('style','margin:10px;cursor:pointer;');
-	qrcode.setAttribute('src','https://api.qrserver.com/v1/create-qr-code/?size=300x300&data=' + decodeURIComponent(qrData));
+	qrcode.setAttribute('src','https://api.isoyu.com/qr/?m=1&e=H&p=10&url=' + decodeURIComponent(qrData));
 	qrcode.setAttribute('title','Click to close.');
 	
 var qrurl = document.createElement("div");
